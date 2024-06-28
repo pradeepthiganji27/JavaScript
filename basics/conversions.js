d108fe0ddbc55a107e80eaf6eba5ea935f2834e2
pradeepthiganji27 <ganjipradeepthi.27@gmail.com>
2024-06-28 07:05:34 +0000

    operations

--- basics/conversions.js
+++ basics/conversions.js
@@ -46,3 +46,8 @@ console.log(1 + "2");//12
 console.log("1" + "2");//12
 console.log("1" + 2 + 2);//122
 console.log(1 + 2 + "2");//32
+
+console.log(true); //true
+console.log(+true); // 1
+console.log(true+); // error
+console.log(+"") // 0
