9d967f917c576300c32f0206e0613b13180f9a38
pradeepthiganji27 <ganjipradeepthi.27@gmail.com>
2024-06-28 06:39:33 +0000

    conversions

--- basics/conversions.js
+++ basics/conversions.js
@@ -10,3 +10,22 @@ console.log(valueInNumber); //33
 let valueInNumber1 = Number(score1)
 console.log(typeof valueInNumber1); // number
 console.log(valueInNumber1); // NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "deepu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
